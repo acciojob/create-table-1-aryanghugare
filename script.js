4f22cafb469546d5012d55b1ba89de2b50471ef4
Aryan Ghugare <aryanghugare75@gmail.com>
2026-01-28 20:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
+	const table = document.querySelector("#sampleTable");
+	const tabr = document.createElement("tr");
+	tabr.innerHTML = `<td>New Cell1</td>
+	<td>New Cell2</td>`
+	table.prepend(tabr); 
   
   
 }
