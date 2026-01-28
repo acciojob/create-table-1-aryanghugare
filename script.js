d75a9211e1d187d5b461b32bf1178d0a709ba564
Aryan Ghugare <aryanghugare75@gmail.com>
2026-01-28 21:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// function insert_Row() {
+//     //Write your code here
+// 	const table = document.querySelector("#sampleTable");
+// 	const tabr = document.createElement("tr");
+// 	tabr.innerHTML = `<td>New Cell1</td>
+// 	<td>New Cell2</td>`
+// 	table.prepend(tabr);  
+  
+  
+// }
+
 function insert_Row() {
-    //Write your code here
-	const table = document.querySelector("#sampleTable");
-	const tabr = document.createElement("tr");
-	tabr.innerHTML = `<td>New Cell1</td>
-	<td>New Cell2</td>`
-	table.prepend(tabr); 
-  
-  
+
+  const tbody = document
+    .getElementById("sampleTable")
+    .tBodies[0];
+
+  const row = document.createElement("tr");
+
+  row.innerHTML = "<td>New Cell1</td><td>New Cell2</td>";
+
+  tbody.prepend(row);
 }
+
